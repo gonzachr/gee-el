275fe2fb2a9e30053486711a1a4700457b23af72
gonza <gonzalo.christobal@gmail.com>
2020-11-02 00:35:04 -0300

    Remove texture

--- src/glsl/fragment.glsl
+++ src/glsl/fragment.glsl
@@ -1,7 +1,7 @@
 #version 330 core
 out vec4 fragColor;
 
-in vec2 v_TexCoord;
+// in vec2 v_TexCoord;
 
 uniform vec4 u_Color;
 
@@ -10,8 +10,8 @@ uniform sampler2D u_Texture2;
 
 void main()
 {
-    vec4 texColor1 = texture(u_Texture1, v_TexCoord);
-    vec4 texColor2 = texture(u_Texture2, v_TexCoord);
-    vec4 texColor = mix(texColor1, texColor2, 0.2);
-    fragColor = texColor;
+    // vec4 texColor1 = texture(u_Texture1, v_TexCoord);
+    // vec4 texColor2 = texture(u_Texture2, v_TexCoord);
+    // vec4 texColor = mix(texColor1, texColor2, 0.2);
+    fragColor = u_Color;
 }
--- src/glsl/vertex.glsl
+++ src/glsl/vertex.glsl
@@ -1,13 +1,13 @@
 #version 330 core
 
 layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec2 vTex;
+// layout(location = 1) in vec2 vTex;
 
-out vec2 v_TexCoord;
+// out vec2 v_TexCoord;
 
 uniform mat4 u_MVP;
 
 void main() {
   gl_Position = u_MVP * vec4(aPos.x, aPos.y, aPos.z, 1.0);
-  v_TexCoord = vTex;
+  // v_TexCoord = vTex;
 }
